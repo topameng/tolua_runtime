3f05f309517a0729c1059b79eae1acb0b4114306
Jonson <596809147@qq.com>
2019-05-13 18:11:01 +0800

    去掉新的xcode不支持的i386架构 添加x86 build用的Android库的绝对路径

--- build_x86.sh
+++ build_x86.sh
@@ -13,7 +13,7 @@ cp ./libluajit.a ../../android/jni/libluajit.a
 make clean
 
 cd ../../android
-ndk-build clean APP_ABI="armeabi-v7a,x86"
-ndk-build APP_ABI="x86"
+$NDK/ndk-build clean APP_ABI="armeabi-v7a,x86"
+$NDK/ndk-build APP_ABI="x86"
 cp libs/x86/libtolua.so ../Plugins/Android/libs/x86
-ndk-build clean APP_ABI="armeabi-v7a,x86"
+$NDK/ndk-build clean APP_ABI="armeabi-v7a,x86"
