026a48952770adf7a1a437fde79d615c2e58bb4b
Jonson <596809147@qq.com>
2019-05-14 17:15:52 +0800

    修正msys64 无法在windows上正常的一次性编译出Android 的arm64的问题

--- build_arm64.sh
+++ build_arm64.sh
@@ -14,8 +14,14 @@ make HOST_CC="gcc -m64" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARC
 cp ./libluajit.a ../../android/jni/libluajit.a
 make clean
 
-cd ../../android
-$NDK/ndk-build clean APP_ABI="armeabi-v7a,x86,arm64-v8a"
-$NDK/ndk-build APP_ABI="arm64-v8a"
-cp libs/arm64-v8a/libtolua.so ../Plugins/Android/libs/arm64-v8a
-$NDK/ndk-build clean APP_ABI="armeabi-v7a,x86,arm64-v8a"
+if [[ "$OSTYPE" == "msys" ]]; then
+	cd ../../
+	# can't pass $NDK to bat
+	cmd /c "link_arm64.bat"
+else
+	cd ../../android
+	$NDK/ndk-build clean APP_ABI="armeabi-v7a,x86,arm64-v8a"
+	$NDK/ndk-build APP_ABI="arm64-v8a"
+	cp libs/arm64-v8a/libtolua.so ../Plugins/Android/libs/arm64-v8a
+	$NDK/ndk-build clean APP_ABI="armeabi-v7a,x86,arm64-v8a"
+fi
